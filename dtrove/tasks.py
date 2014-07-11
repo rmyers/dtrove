2da9bb86f369d13e9ed28192cde3ffbc7dad2c04
Robert Myers <myer0052@gmail.com>
2014-07-10 23:20:48 -0500

    Updating docs

--- dtrove/tasks.py
+++ dtrove/tasks.py
@@ -6,8 +6,58 @@ These handle all the remote operations that happen. Such as building a new
 cluster, or run a scheduled backup. We are using celery as our worker
 daemon, giving us a powerful system to schedule or chain tasks together.
 
-By chaining tasks together, we can have a simple workflow pattern and can
-trigger a roll back if needed.
+Execution Tasks
+---------------
+
+We can use the `perform` task to run any command on the remote systems.
+
+.. py:function:: dtrove.tasks.perform(instance_id, name, *cmds)
+
+    Execute a remote task
+
+    :param int instance_id: ID of the instance to connect to
+    :param str name: Identifier of this task
+    :param str cmds: The actual commands to run
+
+    For example (To ruin someones day)::
+
+         perform.delay(1, 'destory', 'rm -rf /', 'echo everything gone')
+
+         Will produce
+         [root@10.0.0.1] run: rm -rf /
+         out:
+         [root@10.0.0.1] run: echo everything gone
+         out: everything gone
+         Disconnecting from 127.0.0.1...
+         done.
+
+Build Tasks
+-----------
+
+These tasks handle creating and managing the servers themselves.
+
+.. py:function:: dtrove.tasks.create_server(instance_id, volume_id=None)
+
+    Create a nova server instance
+
+    :param int instance_id: ID of the instance to build
+    :param str volume_id: Optional volume id to attach
+
+
+.. py:function:: dtrove.tasks.create_volume(instance_id)
+
+    Create a volume for the instance
+
+    :param int instance_id: ID of the instance
+
+
+.. py:function:: dtrove.tasks.attach_volume(instance_id, volume_id)
+
+    Attach a volume to a nova server instance
+
+    :param int instance_id: ID of the instance to build
+    :param str volume_id: Optional volume id to attach
+
 """
 
 from __future__ import absolute_import
@@ -37,23 +87,10 @@ def xsum(numbers):
 
 @shared_task
 def preform(instance_id, name, *cmds):
-    """Connects to the instance and preforms the commands
-
-    :param int instance_id: ID of the instance to connect to
-    :param str name: Identifier of this task
-    :param cmds: The actual commands to run
-
-    For example::
-
-         perform.delay(1, 'destory', 'rm -rf /', 'echo everything gone')
-
-         Will produce
-         [root@10.0.0.1] output: rm -rf /
-         [root@10.0.0.1] output: everything gone
-    """
+    """Connects to the instance and preforms the commands"""
     instance = Instance.objects.get(pk=instance_id)
 
-    with prefix(config.DTROVE_PREFIX), settings(**instance.connection_info):
+    with settings(**instance.connection_info):
         map(run, cmds)
 
     # Always remember to disconnect ssh sessions
