6530a99239a3d43c67f0a6f2340eb2064872d72e
Robert Myers <myer0052@gmail.com>
2014-07-27 13:05:39 -0500

    Adding tests for base manager class

--- dtrove/tests.py
+++ dtrove/tests.py
@@ -347,3 +347,17 @@ class OpenStackProviderTests(DtroveTest):
             name=self.instance.key.name,
             public_key=self.instance.key.public
         )
+
+
+class BaseDatastoreTests(DtroveTest):
+
+    def setUp(self):
+        from dtrove.datastores.base import BaseManager
+        self.datastore = create_datastore()
+        self.manager = BaseManager(self.datastore)
+
+    def test_backup(self):
+        self.assertRaises(NotImplementedError, self.manager.backup, None)
+
+    def test_prepare(self):
+        self.assertRaises(NotImplementedError, self.manager.prepare, None)
