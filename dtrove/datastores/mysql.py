44b63bb67ab12acd79a27e034b52bfe3f703687b
Robert Myers <myer0052@gmail.com>
2014-07-28 22:39:02 -0500

    Switch to using cStringIO rather than temp files

--- dtrove/datastores/mysql.py
+++ dtrove/datastores/mysql.py
@@ -4,8 +4,7 @@ Oracle MySQL Manager
 
 """
 
-import os
-import tempfile
+from cStringIO import StringIO
 
 from fabric.api import run, env, put
 
@@ -21,9 +20,7 @@ class MySQLManager(base.BaseManager):
         for pkg in self.packages:
             run('DEBIAN_FRONTEND=noninteractive apt-get install -y %s' % pkg)
 
-        with tempfile.NamedTemporaryFile() as config:
-            config.write(self.render_config_file(instance))
-            config.flush()
-            put(config, '/etc/mysql/my.cnf')
+        config = StringIO(self.render_config_file(instance))
+        put(config, '/etc/mysql/my.cnf')
 
         self.restart()
